7993c63c67e52c791293fc6ac44eca47266154e2
Connor Green <cgreen@launchdarkly.com>
2024-04-15 19:26:23 -0400

    create branch remove un-needed dep

--- migrate.ts
+++ migrate.ts
@@ -7,8 +7,7 @@ import {
   getJson,
   ldAPIPatchRequest,
   ldAPIPostRequest,
-  rateLimitRequest,
-  delay
+  rateLimitRequest
 } from "./utils.ts";
 import * as Colors from "https://deno.land/std/fmt/colors.ts";
 
